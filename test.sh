be6f5139962627ae4bd7735f9283d1fe6b880cc0
terrier989 <terrier989@gmail.com>
2020-03-28 17:22:16 +0000

    Improves a test script.

--- test.sh
+++ test.sh
@@ -3,7 +3,13 @@
 set -e
 cd `dirname $0`
 
+# Test with Dart SDK
 ./all.sh pub get
 ./all.sh pub run test
+
+# Test with Flutter SDK
 ./all.sh flutter pub get
 ./all.sh flutter pub run test --platform=vm
+
+# Restore Dart SDK dependencies
+./all.sh pub get --offline
