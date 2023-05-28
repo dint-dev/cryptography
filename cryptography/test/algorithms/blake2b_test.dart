7815649f8dda56f03adec45667b3457f72ee19b3
Andreas Kecht <104160893+akecht@users.noreply.github.com>
2023-05-28 22:41:03 +0200

    added additional test cases for blake2b

--- cryptography/test/algorithms/blake2b_test.dart
+++ cryptography/test/algorithms/blake2b_test.dart
@@ -152,5 +152,83 @@ BA 80 A5 3F 98 1C 4D 0D 6A 27 97 B6 9F 12 F6 E9
         );
       }
     });
+    test('63 bytes input', () async {
+      final expectedBytes = hexToBytes(
+          '058460852577e7de15323b6dbfc3656b325dc67a608fa555cfd7694b64a3433d3088eb9572fca9b7e776801bf032f84e179dbec34361f5edb47128d0cf236459');
+      // hash()
+      {
+        final hash = await algorithm.hash(utf8.encode(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit turpis.'));
+        expect(
+          hexFromBytes(hash.bytes),
+          hexFromBytes(expectedBytes),
+        );
+      }
+    });
+    test('64 bytes input', () async {
+      final expectedBytes = hexToBytes(
+          'fc32fd5f4a3c6859cb6779cb14b277dd8483ae6da7fb593b62c4387a3dfe0f22fe96592b1affc725c2fd37f8d09ed5d096af377bf3b2453746d4fa79906d3221');
+      // hash()
+      {
+        final hash = await algorithm.hash(utf8.encode(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit aliquam.'));
+        expect(
+          hexFromBytes(hash.bytes),
+          hexFromBytes(expectedBytes),
+        );
+      }
+    });
+    test('65 bytes input', () async {
+      final expectedBytes = hexToBytes(
+          'c8fa7cb677e3331450b508a4920338719c36709acfb09b6f6cab2dc2bbef0d292caddf6c444b0245c9dcab77c2fddaef89176acebc4e684e3caf6e853d5ac3ed');
+      // hash()
+      {
+        final hash = await algorithm.hash(utf8.encode(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit volutpat.'));
+        expect(
+          hexFromBytes(hash.bytes),
+          hexFromBytes(expectedBytes),
+        );
+      }
+    });
+    test('127 bytes input', () async {
+      final expectedBytes = hexToBytes(
+          '4b4f18b171f48aae4159ca84129d7d03bc36cd79d89157ac80e7fdc747754b361c3928ca246e925cedd5ef4712d2aa8f21b39953b724410239da00327a040a0f');
+      // hash()
+      {
+        final hash = await algorithm.hash(utf8.encode(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ullamcorper tellus risus, a dapibus sapien hendrerit eget eros.'));
+        expect(
+          hexFromBytes(hash.bytes),
+          hexFromBytes(expectedBytes),
+        );
+      }
+    });
+    test('128 bytes input', () async {
+      final expectedBytes = hexToBytes(
+          '7a5164b11f34301dc9a95b06f610d4b2fa6b7ccbe51112800e6db10890319a8fcda37ec4d50ea25001cf7b493946f2403579a852e6c44c573da3eab9bf2b0a38');
+      // hash()
+      {
+        final hash = await algorithm.hash(utf8.encode(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus arcu dui. Fusce sed sem lectus. Cras mi massa porta ante.'));
+        expect(
+          hexFromBytes(hash.bytes),
+          hexFromBytes(expectedBytes),
+        );
+      }
+    });
+    test('444 bytes input', () async {
+      final expectedBytes = hexToBytes(
+          '8c12a0cf10e32bd38c8f458f880986609895988b013b0ca6ecf76aeb3dab2bebe8123a64c9f777acfebcca240c57e2788301ce6ae3ce30d20518a2f8d4002e9c');
+      // hash()
+      {
+        final hash = await algorithm.hash(utf8.encode(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus commodo, ante id consectetur mollis, urna felis rutrum sem, nec dictum magna ligula quis nisl. Donec varius tempus pharetra. Cras auctor molestie ornare. Cras vestibulum, nibh in tempus finibus, arcu est sagittis lectus, bibendum ullamcorper dolor sapien ac turpis. Nulla a commodo purus. Integer elit ipsum, mattis a egestas et, auctor quis diam. Curabitur placerat massa nam.'));
+        expect(
+          hexFromBytes(hash.bytes),
+          hexFromBytes(expectedBytes),
+        );
+      }
+    });
   });
 }
